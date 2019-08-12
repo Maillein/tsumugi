0fdb28ba44a1226a960722cad2e8c2c4a7b57af5
MShinya <realshinya1999@gmail.com>
2019-08-13 01:09:38 +0900

    Todoリストの最初のコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './App.scss';
 
+interface TodoProps {
+    id: number,
+    title: string,
+    content: string,
+    done: boolean
+}
+
+class Todo extends React.Component {
+    constructor(props: TodoProps) {
+        super(props);
+    }
+}
+
 const App: React.FC = () => {
   return (
     <div className="App">
